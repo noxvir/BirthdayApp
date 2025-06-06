eefcbd30edea908c4a893f9de91e11b0159f828b
Dean <sdean4317@gmail.com>
2025-06-06 10:04:57 -0500

    Initial Commit

---
+++ BirthdayApp/BirthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayAppApp.swift
+//  BirthdayApp
+//
+//  Created by Scholar on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
